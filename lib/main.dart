7d38a7618442ae92d526fc68ca51da665d26e600
phipfouxie <yasli_sad51@mail.ru>
2024-01-29 13:18:13 +0600

    jeeon

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,16 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(appBar: AppBar(backgroundColor: Colors.pink,),
-       body: Container(color: Colors.purple,child: Text("hi")),)
-    );
+    return Container(color: Colors.red,);
   }
 }
